435c0e56f178422bfb9dd71622718bb371e5f5c4
Sandun Meesara <154401052+sandunmeesara@users.noreply.github.com>
2025-02-01 21:25:41 +0530

    servo motor and ultrasonic functioning codes

--- Robot_Car_Steppers.ino
+++ Robot_Car_Steppers.ino
@@ -1,5 +1,12 @@
 #include <AccelStepper.h>
+#include <Servo.h>
+
+Servo myservo;  // create Servo object to control a servo
+int pos = 0;    // variable to store the servo position
+
 #define MOTOR_INTERFACE_TYPE 4
+#define TRIG_PIN A0  // Use A0 as a digital output
+#define ECHO_PIN A1  // Use A1 as a digital input
 
 // Define some steppers and the pins the will use
 AccelStepper stepper1(MOTOR_INTERFACE_TYPE, 2, 3, 4, 5);
@@ -7,6 +14,8 @@ AccelStepper stepper2(MOTOR_INTERFACE_TYPE, 6, 7, 8, 9);
 
 // Define variables
 bool isMoving = false;
+unsigned long previousMillis = 0; // Stores last time event occurred
+const long interval = 100; // Time interval in milliseconds (1 second)
 
 // Steps per degree (4096 steps per revolution / 360 degrees)
 const float STEPS_PER_DEGREE = 4096.0 / 360.0;
@@ -14,6 +23,10 @@ const float STEPS_PER_DEGREE = 4096.0 / 360.0;
 void setup()
 {  
     Serial.begin(9600);
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    myservo.attach(10);  // attaches the servo on pin 9 to the Servo object
+
     stepper1.setMaxSpeed(1000.0);
     stepper1.setAcceleration(1000.0);
     stepper1.moveTo(1000000);
@@ -25,6 +38,36 @@ void setup()
 
 void loop()
 {
+    unsigned long currentMillis = millis(); // Get current time
+
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis; // Update last event time
+      digitalWrite(TRIG_PIN, LOW);
+      delayMicroseconds(2);
+      digitalWrite(TRIG_PIN, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(TRIG_PIN, LOW);
+
+      long duration = pulseIn(ECHO_PIN, HIGH);
+      float distance = duration * 0.034 / 2;
+
+      Serial.print("Distance: ");
+      Serial.print(distance);
+      Serial.println(" cm");
+
+      if(distance <= 20){
+        for (pos = 40; pos <= 140; pos += 1) { // goes from 0 degrees to 180 degrees
+          // in steps of 1 degree
+          myservo.write(pos);              // tell servo to go to position in variable 'pos'
+          delay(15);                       // waits 15 ms for the servo to reach the position
+        }
+        for (pos = 140; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+          myservo.write(pos);              // tell servo to go to position in variable 'pos'
+          delay(15);                       // waits 15 ms for the servo to reach the position
+        }
+      }
+    }
+
     // Check for serial commands
   if (Serial.available() > 0) {
     char command = Serial.read();
